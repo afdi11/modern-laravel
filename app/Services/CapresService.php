4c2b5bd4230cbffe4da5b4356f286125d65c0b86
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2024-02-02 23:25:22 +0700

    handle error http request

--- app/Services/CapresService.php
+++ app/Services/CapresService.php
@@ -2,13 +2,21 @@
 
 namespace App\Services;
 
+use Exception;
 use Illuminate\Support\Facades\Http;
+use RuntimeException;
 
 class CapresService
 {
     public static function apiCapresku(): Array
     {
-        $response = Http::get("https://mocki.io/v1/92a1f2ef-bef2-4f84-8f06-1965f0fca1a7");
+        try{
+            $response = Http::get("https://mocki.io/v1/92a1f2ef-bef2-4f84-8f06-1965f0fca1a7");
+        }catch(RuntimeException $e){
+            return [];
+        }catch(Exception $e){
+            return [];
+        }
 
         return $response->json() ?? [];
     }
