109112c1b9d3effae2e88e4ffc10e46f3d14f453
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2024-02-03 08:14:35 +0700

    Update Function parseKarir

--- app/Services/CapresService.php
+++ app/Services/CapresService.php
@@ -4,11 +4,12 @@ namespace App\Services;
 
 use Exception;
 use Illuminate\Support\Facades\Http;
+use App\Data\Karir;
 use RuntimeException;
 
 class CapresService
 {
-    public static function apiCapresku(): Array
+    public static function apiCapresku(): array
     {
         try{
             $response = Http::get("https://mocki.io/v1/92a1f2ef-bef2-4f84-8f06-1965f0fca1a7");
@@ -21,9 +22,53 @@ class CapresService
         return $response->json() ?? [];
     }
 
-    public static function parseTanggalLahir(string $tempatTanggalLahir):string
-    {
+    public static function parseTanggalLahir(string $tempatTanggalLahir): string {
         $tempatTanggalLahir = explode(", ", $tempatTanggalLahir);
-        return $tempatTanggalLahir[1] ?? "";
+        return is_array($tempatTanggalLahir) && isset($tempatTanggalLahir[1]) ? $tempatTanggalLahir[1] : "";
+    }
+
+    public static function parseKarir(array $karir):array
+    {
+        $data = CapresService::apiCapresku();
+        $karir = $data['calon_presiden'][2]['karir'];
+        $newKarir = array();
+        foreach ($karir as $value) {
+            //Checking
+            if($value != "" && $value != null){
+                //explode data
+                $explodeKarir = CapresService::parseKarirDariString($value);
+                $newKarir = array_merge($newKarir, $explodeKarir);
+            }
+        }
+        return $newKarir;
+    }
+
+    public static function parseKarirDariString(string $data) {
+        [$jabatanPart, $yearsPart] = explode('(', $data, 2);
+        $jabatan = trim($jabatanPart);
+        $yearsPart = rtrim($yearsPart, ")");
+
+        $result = array();
+
+        //Cek Periode
+        if (strpos($yearsPart, " dan ") !== false) {
+            $periods = explode(" dan ", $yearsPart);
+        } else if (strpos($yearsPart, "/") !== false){
+            $periods = explode("/", $yearsPart);
+        }else{
+            $periods = array($yearsPart);
+        }
+
+        foreach ($periods as $period) {
+            // Mengurai masing-masing periode
+            preg_match('/(\d{4})-(\d{4})/', $period, $matches);
+            $result[] = new Karir(
+                $jabatan ?? "",
+                intval($matches[1]),
+                intval($matches[2])
+            );
+        }
+
+        return $result;
     }
 }
